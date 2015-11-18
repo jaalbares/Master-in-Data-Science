b6cc1a71fa45153a0258560dc65515340d2f239e
Jose Antonio Albares <jalbares@gmail.com>
2015-11-18 23:08:06 +0100

    Inclusión en ejercicio2_20151118.sh de control de errores para parámetros vacíos

--- ejercicio2_20151118.sh
+++ ejercicio2_20151118.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+
+if [ "$#" -eq "2" ]
+then
 csvgrep -d '^' -c nb_engines -m $1 $2|tail -n +2|wc -l
+else 
+echo "ERROR: Usage: ./ejercicio2_20151115.sh <num_engines> <aircraft_file.csv>"
+fi
